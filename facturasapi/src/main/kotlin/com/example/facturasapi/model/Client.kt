a1c4ac707b5a1a8a0f7eb978fa1e21c99cfaeda0
luis05t <latinoco@sudamericano.edu.ec>
2024-10-15 19:33:19 -0500

    luis1

--- facturasapi/src/main/kotlin/com/example/facturasapi/model/Client.kt
+++ facturasapi/src/main/kotlin/com/example/facturasapi/model/Client.kt
@@ -3,13 +3,19 @@ package com.example.facturasapi.model
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "client")
-class Client {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@Table(name = "client")  // Cambiado a 'client' en singular
+class Client(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false)
-    var id: Long? = null
-    var nui: String? = null
-    var fullname: String? = null
+    var id: Long? = null,
+
+    @Column(nullable = false, length = 13, unique = true)
+    var nui: String? = null,
+
+    @Column(nullable = false, length = 100)
+    var fullname: String? = null,
+
+    @Column(length = 50)
     var address: String? = null
-}
+)
